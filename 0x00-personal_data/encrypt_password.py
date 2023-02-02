1e9f1a7a4fcabeb02505e4010ef8364a0352fa45
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-02-02 14:59:22 +0100

    fix

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,7 +3,7 @@
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     """a function called hash_password that takes a password
     string arguments and returns a salted, hashed password,
     which is a byte string"""
