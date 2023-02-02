2c27c01d817e77481010c4f1d05087a1d729e6c7
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-02-02 12:35:08 +0100

    Fix: req issue

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """"0. Regex-ing"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """a function called filter_datum that returns the
     log message obfuscated"""
     for field in fields:
