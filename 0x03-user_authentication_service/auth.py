d00c809dd473301a403bd5e04bbc99ee29723dd6
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-02-13 19:16:14 +0100

    Add advanced task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -81,8 +81,9 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
+            print("user", user)
             token = self._generate_uuid()
-            self._db.update_user(user.id, reset_password_token=token)
+            self._db.update_user(user.id, reset_token=token)
             return token
         except NoResultFound:
             raise ValueError
