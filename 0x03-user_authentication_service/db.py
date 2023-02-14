cea0dcc4242d7a32beab501a1d79342d1854ec5c
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-02-14 08:29:44 +0100

    why aren't you working?

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -17,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+        self._engine = create_engine("sqlite:///a.db", echo=True)
         # set echo to False to not see QUERY STATEMENT output
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
