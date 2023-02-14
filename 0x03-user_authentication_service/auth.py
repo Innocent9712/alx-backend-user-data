f52e8d5ea6419d94304d006e694a7ca39e349f5b
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-02-14 09:43:59 +0100

    remove print statement

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -82,7 +82,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            print("user", user)
+            # print("user", user)
             token = _generate_uuid()
             self._db.update_user(user.id, reset_token=token)
             return token
