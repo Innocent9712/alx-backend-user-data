02137a9e38bf13d05a8cbc9e10b86692f4a92b4e
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-02-14 08:57:01 +0100

    Fix: wrong return type

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -22,7 +22,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email: str, password: str) -> None:
+    def register_user(self, email: str, password: str) -> User:
         """Register a user
         """
         try:
